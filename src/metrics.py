02c1608b5bfe7d4223d12d3f564d45b37697ab07
einhornus <luckjanovdmitry@yandex.ru>
2023-05-14 15:34:49 +0400

    grammar correction research

--- src/metrics.py
+++ src/metrics.py
@@ -10,6 +10,8 @@ def calculate_metric(expected, actual, metric):
 def calculate_bleu(expected, actual):
     tokenized_reference = word_tokenize(expected.lower())
     tokenized_candidate = word_tokenize(actual.lower())
+    tokenized_reference = [e for e in tokenized_reference if e not in ('.', ',', '?', '!')]
+    tokenized_candidate = [e for e in tokenized_candidate if e not in ('.', ',', '?', '!')]
     reference_list = [tokenized_reference]
     bleu_score = sentence_bleu(reference_list, tokenized_candidate)
     return bleu_score
--- src/visualize.py
+++ src/visualize.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import os
 import json
-import numpy as np
 
 
-def visualize(project, system_message_files, models, parameter_configurations, ks, metric):
+def visualize(project, system_message_files, models, parameters_configurations, ks, metric):
     report_files = os.listdir("..//data//" + project + "//reports//")
-    report_files.sort(key = lambda x: int(x.split(" ")[-2].replace("k=", "")))
+    report_files.sort(key=lambda x: int(x.split(" ")[-2].replace("k=", "")))
 
     data = []
     labels = []
@@ -21,11 +20,11 @@ def visualize(project, system_message_files, models, parameter_configurations, k
         if report["metric"] == metric:
             if report["system_message_file"] in system_message_files:
                 if report["model"] in models:
-                    if report["parameters"] in parameter_configurations:
+                    if report["parameters"] in parameters_configurations:
                         if report["k"] in ks:
                             data_column = []
                             for test in report["tests"]:
-                                data_column.append(test["metric"])
+                                data_column.append(test["score"])
                             data.append(data_column)
                             system_messages_set.add(report["system_message_file"])
                             models_set.add(report["model"])
@@ -39,47 +38,125 @@ def visualize(project, system_message_files, models, parameter_configurations, k
                             parameter_configurations_set.add(string_parameter_list)
                             ks_set.add(report["k"])
                             labels.append(
-                                (report["system_message_file"], report["model"], string_parameter_list, report["k"], report["average"], report["std"], report["median"]))
+                                (report["system_message_file"], report["model"], string_parameter_list, report["k"],
+                                 report["average"], report["std"], report["median"]))
 
     common_label = project + "\n"
     if len(system_messages_set) == 1:
-        common_label += "system="+list(system_messages_set)[0] + " "
+        common_label += "system=" + list(system_messages_set)[0] + " "
     if len(models_set) == 1:
-        common_label += "model="+list(models_set)[0] + " "
+        common_label += "model=" + list(models_set)[0] + " "
     if len(parameter_configurations_set) == 1:
-        common_label += "p={"+list(parameter_configurations_set)[0] + "} "
+        common_label += "p={" + list(parameter_configurations_set)[0] + "} "
     if len(ks_set) == 1:
-        common_label += "k="+str(list(ks_set)[0]) + " "
+        common_label += "k=" + str(list(ks_set)[0]) + " "
 
     string_labels = []
     for label in labels:
         string_label = f"{label[4]:.3f} ± {label[5]:.3f}\n"
         string_label += f"med = {label[6]:.3f}\n"
         if len(system_messages_set) > 1:
-            string_label += "system="+label[0] + " "
+            string_label += "system=" + label[0] + " "
         if len(models_set) > 1:
-            string_label += "model="+label[1] + " "
+            string_label += "model=" + label[1] + " "
         if len(parameter_configurations_set) > 1:
-            string_label += "p={"+label[2] + "} "
+            string_label += "p={" + label[2] + "} "
         if len(ks_set) > 1:
-            string_label += "k="+str(label[3]) + " "
+            string_label += "k=" + str(label[3]) + " "
         string_labels.append(string_label)
 
     plt.figure(figsize=(10, 6))
     plt.style.use('ggplot')
     bp = plt.boxplot(data, patch_artist=True, notch=True)
-    colors = ['lightblue', 'lightgreen', 'pink', 'purple']
     for element in ['whiskers', 'caps', 'fliers', 'medians']:
         plt.setp(bp[element], color='black')
-    plt.xticks(np.arange(1, len(labels) + 1), string_labels)
+    plt.xticks([i for i in range(1, len(labels) + 1)], string_labels)
     plt.title(common_label)
-    for i in range(len(labels)):
-        plt.plot([], label=string_labels[i])
-    # plt.legend()
+    plt.ylabel(metric)
     plt.show()
 
 
 if __name__ == "__main__":
+    visualize("grammar_correction",
+              ["1", "2", "3", "4", "5", "6"],
+              ["gpt-3.5-turbo"],
+              [
+                  {"language": "English"},
+              ],
+              [0],
+              "bleu"
+              )
+
+    visualize("grammar_correction",
+              ["1", "2", "3", "4", "5", "6"],
+              ["gpt-3.5-turbo"],
+              [
+                  {"language": "English"},
+              ],
+              [2000],
+              "bleu"
+              )
+    exit(0)
+
+    visualize("grammar_correction",
+              ["4"],
+              ["gpt-3.5-turbo"],
+              [
+                  {"language": "English"},
+                  {"language": "German"},
+                  {"language": "French"},
+                  {"language": "Spanish"},
+                  {"language": "Dutch"},
+                  {"language": "Russian"},
+              ],
+              [0],
+              "bleu"
+              )
+    exit(0)
+
+    visualize("grammar_correction",
+              ["4"],
+              ["gpt-3.5-turbo", "gpt-4"],
+              [
+                  {"language": "English"},
+              ],
+              [0, 2000],
+              "bleu"
+              )
+
+    exit(0)
+
+    visualize("grammar_correction",
+              ["4"],
+              ["gpt-3.5-turbo"],
+              [
+                  {"language": "English"},
+              ],
+              [0, 500, 1000, 2000],
+              "bleu"
+              )
+    exit(0)
+
+    visualize("grammar_correction",
+              ["1", "2", "3", "4", "5", "6"],
+              ["gpt-3.5-turbo"],
+              [
+                  {"language": "English"},
+              ],
+              [0],
+              "bleu"
+              )
+
+    visualize("grammar_correction",
+              ["1", "2", "3", "4", "5", "6"],
+              ["gpt-3.5-turbo"],
+              [
+                  {"language": "English"},
+              ],
+              [2000],
+              "bleu"
+              )
+
     """
     visualize("grammar_correction",
               ["improve"],
@@ -120,6 +197,7 @@ if __name__ == "__main__":
               )
     """
 
+    """
     visualize("grammar_correction",
               ["improve"],
               ["gpt-4"],
@@ -129,3 +207,4 @@ if __name__ == "__main__":
               [0, 500, 1000],
               "bleu"
               )
+    """
